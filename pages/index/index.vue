2ed15b0521aaf2958548afeaff24147beb20feb6
ElevenVar <18380214028@163.com>
2019-09-09 23:40:35 +0800

    上传图片

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,6 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<image class="logo" src="/static/img/loginImg/1080.1920.png"></image>
 	</view>
 </template>
 
@@ -24,29 +21,13 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+	uni-page-body{
+		height:100%;
 	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50upx;
+	uni-view{
+		height:100%;
 	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
+	uni-image{
+		width:100%;height:100%;
 	}
 </style>
